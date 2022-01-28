2497ed11221d804ad848230706c2306b0c071747
rockvns <chandrabhanvns@gmail.com>
2022-01-28 16:53:24 +0530

    making frontend independent for easy front end development

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 // import { Provider } from 'react-redux'
 import App from './App'
-import '../static/scss/style.scss'
+import './static/scss/style.scss'
 ReactDOM.render(
     
         
