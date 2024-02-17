838d723fb443e41c18b0808cf8b1a11ce6abdafc
rockvns <chandrabhanvns@gmail.com>
2024-02-17 19:53:01 +0530

    create react app with vite

---
+++ djReact/management/commands/__init__.py
@@ -0,0 +1,12 @@
+APP_INITIALIZER_CONTENT="""import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Your React App!</h1>
+      <p>Edit src/App.js to get started.</p>
+    </div>
+  );
+}
+
+export default App;"""
--- djReact/management/commands/startproject.py
+++ djReact/management/commands/startproject.py
@@ -1,18 +1,18 @@
-# myapp/management/commands/generate_template.py
+# app/management/commands/generate_template.py
 import os
 import shutil
 from sys import stdout
+import sys
 from django.core.management.base import BaseCommand
+
+from djReact.management.commands import APP_INITIALIZER_CONTENT
 
 
 class Command(BaseCommand):
     help = "Generates a Django project with React integration"
 
-    def add_arguments(self, parser):
-        parser.add_argument("project_name", type=str, help="Name of the Django project")
-
-    def handle( *args, **kwargs):
-        project_name = kwargs["project_name"]
+    def handle(*args, **kwargs):
+        project_name = sys.argv[1]
 
         # Create a new Django project
         os.system(f"django-admin startproject {project_name}")
@@ -21,34 +21,27 @@ class Command(BaseCommand):
         os.chdir(project_name)
 
         # Create a new Django app
-        os.system("python manage.py startapp myapp")
+        os.system("python manage.py startapp app")
 
         # Navigate into the newly created app directory
-        os.chdir("myapp")
+        os.chdir("app")
 
         # Create a new React app
-        os.system("npx create-react-app frontend")
+        os.system("npx create-vite@latest frontend --template react")
 
         # Navigate into the React app directory
         os.chdir("frontend")
+        src_dir = os.path.join(os.getcwd(), "src", "App.jsx")
 
+        while not os.path.exists(src_dir):
+            break
+        with open(src_dir, "w") as app:
+            app.write(APP_INITIALIZER_CONTENT)
         # Copy template files for React
-        src_dir = os.path.join(os.getcwd(), "src")
-        shutil.rmtree(
-            src_dir
-        )  # Remove the default src directory created by create-react-app
-        os.mkdir(src_dir)
-
-        # Copy template files for React
-        templates_dir = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "templates"
-        )
-        shutil.copytree(templates_dir, src_dir)
 
         # Navigate back to the project directory
         os.chdir("..")
 
         stdout.write(
-            
-                f"Successfully created Django project with React integration: {project_name}"
+            f"Successfully created Django project with React integration: {project_name}"
         )
