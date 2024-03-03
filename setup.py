08e134831e090c89fc85fb2aed6ffc3172f03afa
chandrabhan maurya <65912538+rockvns@users.noreply.github.com>
2024-03-03 17:25:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="djReact",
-    version="1.0",
+    version='{{VERSION_PLACEHOLDER}}',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
