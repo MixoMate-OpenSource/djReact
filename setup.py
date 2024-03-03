debd928021165ee645025eb5b9b8aaef81e18fee
rockvns <chandrabhanvns@gmail.com>
2024-03-03 17:43:58 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='{{VERSION_PLACEHOLDER}}',
     packages=find_packages(),
     include_package_data=True,
+    long_description_content_type="text/markdown",
     install_requires=[
         "Django>=5.0",
     ],
