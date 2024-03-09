f616e38745ec31bda44589dbd59266a7543fbc72
rockvns <chandrabhanvns@gmail.com>
2024-03-10 00:19:20 +0530

    frontend app renamed

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    description = f.read()
+
 setup(
     name="djReact",
-    version="2.1",
+    version="2.1.1",
     packages=find_packages(),
     include_package_data=True,
-    long_description_content_type="text/markdown",
     install_requires=["Django>=3.0"],
     entry_points={
         "console_scripts": [
             "djReact = djReact.management.commands.startproject:Command.handle",
         ],
     },
-    data_files=[("data", ["djReact/frontend/frontend.zip","README.md", "README.md"])],
+    data_files=[("data", ["djReact/frontend/frontend.zip"])],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.10",
+    long_description=description,
+    long_description_content_type="text/markdown",
 )
